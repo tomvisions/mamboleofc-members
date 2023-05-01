fb7740733ee5bf11b67ad89aa48bd3a6adc6e492
Tom <tomc@tomvisions.com>
2023-05-01 08:42:32 -0400

    adding link

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -24,7 +24,7 @@ export class SharedModule implements OnInit
             this._apiLocation = 'http://127.0.0.1:3000'
         } else if (environment.node_env === 'stage') {
        //     this._apiLocation = 'https://gv3y9o6cej.execute-api.us-east-1.amazonaws.com/stage'
-            this._apiLocation = 'https://tmctzoaqdc.execute-api.us-east-1.amazonaws.com/stage';
+            this._apiLocation = 'https://8ocn61mmp3.execute-api.us-east-1.amazonaws.com/stage';
    //         this._apiLocation = 'https://api-stage.mamboleofc.ca'
         } else {
             this._apiLocation = 'https://api.mamboleofc.ca';
